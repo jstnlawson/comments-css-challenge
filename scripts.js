03a387ecf41dcc347bf0f407067f2605f89b179e
Justin Lawson <jstn.lawson@gmail.com>
2023-07-02 10:51:44 -0500

    finished reply box styling and hover functions

--- scripts.js
+++ scripts.js
@@ -1,14 +1,42 @@
-let num = 0;
- 
-//get the default number
-document.getElementsByClassName("number-display").innerText = num;
- 
-function addOne() {
- num = num + 1;
-    document.getElementByClassName("number-display").innerText = num;
-}
+// I'm still not sure when it's best to use id vs class with this sort 
+// page. In retrospect I'd probably go with class on everything aside
+// from maybe the submit button
 
-function minusOne() {
- num = num - 1;
-    document.getElementByClassName("number-display").innerText = num;
-}
+//I'd also just add jQuery because doing this the old way was very
+//confusing to go back to
+
+function hoverButtonId(buttonId) {
+    var button = document.getElementById(buttonId);
+  
+    button.addEventListener('mouseover', function() {
+      this.style.opacity = '0.5';
+    });
+  
+    button.addEventListener('mouseout', function() {
+      this.style.opacity = '1';
+    });
+  }
+
+  hoverButtonId('send-btn');
+  hoverButtonId('delete-btn');
+  hoverButtonId('edit-btn');
+
+  function hoverButtonClass(buttonClass) {
+    let buttons = document.getElementsByClassName(buttonClass)
+  
+    for (let button of buttons) {
+      button.addEventListener('mouseover', function() {
+        this.style.opacity = '0.5'
+      });
+  
+      button.addEventListener('mouseout', function() {
+        this.style.opacity = '1'
+      })
+    }
+  }
+
+  hoverButtonClass('reply-button')
+  hoverButtonClass('plus-btn')
+  hoverButtonClass('minus-btn')
+
+  //update number
